d6200d2807473066c555a90c9c586669e0848559
waiyeong <waiyeonglee@gmail.com>
2026-03-04 00:19:25 +0800

    callback edits

--- main_with_callback.py
+++ main_with_callback.py
@@ -1,5 +1,17 @@
 import time
+import os
+import argparse
+import pandas as pd
+import numpy as np
 from moomoo import *
+
+trade_env = TrdEnv.SIMULATE
+SHORT_WINDOW = 3
+LONG_WINDOW = 15
+SYMBOL = "HK.00700"
+PROFIT_PCT = 0
+buy_cond = 'FIXED_PROFIT_PCT'
+QTY = 100
 
 # --- Strategy class ---
 class Strategy:
@@ -71,15 +83,41 @@ class OrderHandler(TradeOrderHandlerBase):
         return RET_OK, data
 
 if __name__ == "__main__":
+    argparser = argparse.ArgumentParser(description='Run the trading bot in live or test mode.')
+    argparser.add_argument('--live', default=False, help='Run the bot in live mode (default is test mode)')
+    args = argparser.parse_args()
+    live_mode = args.live
+    try:
+        today_date = pd.Timestamp.today()
+        # today_date = pd.Timestamp("2026-02-22")
+        output_df = pd.DataFrame(main(today_date))
+        if len(output_df):
+            sell_df = output_df.loc[output_df['action'] == 'SELL']
+            total_pct = compute_total_return(sell_df)
+            print(f"Total Return: {total_pct*100:.3f}%")
+
+    except KeyboardInterrupt:
+        print("Manual stop detected.")
+
+    except Exception as e:
+        print("Unexpected error occurred.")
+        print(traceback.format_exc())
+
+    finally:
+        if len(output_df):
+            output_path = os.path.join(os.getcwd(), 'logs', f"{today_date.strftime('%Y-%m-%d %H:%M:%S')} - logs.csv")
+            output_df.to_csv(output_path)
+
+if __name__ == "__main__":
     strategy = Strategy()
 
     # --- Quote context for K-line ---
-    quote_ctx = OpenQuoteContext(host='127.0.0.1', port=11111)
+    quote_ctx = OpenQuoteContext(host="127.0.0.1", port=11111)
     quote_ctx.set_handler(KlineHandler(strategy))
-    quote_ctx.subscribe(['US.AAPL'], [SubType.K_1M])  # 1-minute candles
+    quote_ctx.subscribe([SYMBOL], [SubType.K_1M])  # 1-minute candles
 
     # --- Trade context for order updates ---
-    trade_ctx = OpenSecTradeContext(host='127.0.0.1', port=11111)
+    trade_ctx = OpenSecTradeContext(filter_trdmarket=TrdMarket.HK, host="127.0.0.1", port=11111, security_firm=SecurityFirm.FUTUSG)
     trade_ctx.set_handler(OrderHandler(strategy))
 
     print("Live system started. Waiting for new candles and order updates...")
